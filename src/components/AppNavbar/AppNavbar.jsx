2009da84885e9932d44de09d52c286c6103494d0
unknown <clapisbryan@gmail.com>
2024-08-06 12:06:45 +0800

    return a navbar log

--- src/components/AppNavbar/AppNavbar.jsx
+++ src/components/AppNavbar/AppNavbar.jsx
@@ -14,14 +14,20 @@ function AppNavbar({ isToken, handleLogout }) {
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
-						<Profile />
-						<Button variant='primary' size='sm' onClick={handleLogout}>Logout</Button>
-						<Nav.Link href="/login">Login</Nav.Link>
-						<Nav.Link href="/register">Register</Nav.Link>
+						{isToken ?
+							<>
+								<Profile />
+								<Button variant='primary' size='sm' onClick={handleLogout}>Logout</Button>
+							</>
+							: <>
+								<Nav.Link href="/login">Login</Nav.Link>
+								<Nav.Link href="/register">Register</Nav.Link>
+							</>
+						}
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
-		</Navbar >
+		</Navbar>
 	);
 }
 
